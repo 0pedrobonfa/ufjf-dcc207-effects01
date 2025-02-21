393ac26ab2b5947268765206db7b80a8a18874ca
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2025-02-20 21:45:52 -0300

    adiciona animação da bolinha verde

--- src/Tela.tsx
+++ src/Tela.tsx
@@ -4,7 +4,30 @@ export default function Tela()
 {
     const [posicao, setPosicao] = useState({x:150,y:75});
     const refCanvas = useRef<HTMLCanvasElement|null>(null);
+    const refAnimation = useRef<number | null>(null);
+    const refMov = useRef({
+        x:0,
+        vx:0,
+        ax:0,
+        t0:0,
+        dt:0
+    })
     useEffect(()=>{
+        refAnimation.current = requestAnimationFrame(desenhaQuadro);
+        return () => {
+            if(refAnimation.current){
+                cancelAnimationFrame(refAnimation.current);
+            };
+        };
+    }
+    ,[]);
+
+    function desenhaQuadro(t:number){
+        const m = refMov.current;
+        m.dt = Math.min(t-m.t0, 32)/1000;
+        m.ax = 10;
+        m.vx +=m.ax*m.dt;
+        m.x += m.vx*m.dt;
         const canvas = refCanvas.current;
         if(!canvas) return;
         const ctx = canvas.getContext("2d");
@@ -12,11 +35,15 @@ export default function Tela()
         ctx.fillStyle = "lightblue";
         ctx.fillRect(0,0,canvas.width, canvas.height);
         ctx.fillStyle = "lightgreen";
-        ctx.ellipse(posicao.x,posicao.y,5,5,0,0,2*Math.PI,false);
+        ctx.beginPath()
+        ctx.ellipse(m.x, posicao.y, 5,5,0,0,2*Math.PI,false);
+        ctx.closePath();
+        ctx.ellipse(m.x,posicao.y,5,5,0,0,2*Math.PI,false);
         ctx.stroke();
         ctx.fill();
-    })
-
+        m.t0 = t;
+        refAnimation.current = requestAnimationFrame(desenhaQuadro);
+    }
 
 
     return (<canvas ref={refCanvas} onClick={()=>{
