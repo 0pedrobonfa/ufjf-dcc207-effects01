b631d9f1082ade70c2436ec128e4616915b81633
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2025-02-24 20:50:25 -0300

    inicia useEffect

--- src/Prompt.tsx
+++ src/Prompt.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type PromptProps={
     onPosicaoChange: (posicao:{x:number;y:number})=>void;
@@ -9,7 +9,11 @@ export default function Prompt({onPosicaoChange}:PromptProps){
     const [entrada, setEntrada] = useState("");
     const [saida, setSaida] = useState("");
     const refInput = useRef<HTMLInputElement>(null);
-    const [lista, setLista] = useState<string[]>([]);
+    const [lista, setLista] = useState<string[]>(["mover 10 10","mover 100 100"]);
+
+    const refRelogio = useRef(0);
+    const [execucao, setExecucao] = useState(false);
+    const [prox, setProx] = useState(0);
 
 
     function onEntradaChange(e: React.ChangeEvent<HTMLInputElement>){
@@ -17,28 +21,58 @@ export default function Prompt({onPosicaoChange}:PromptProps){
         //e.target.select();
     }
 
-    function executaLista(){
-        console.log(lista);
-        for(let i=0;i<lista.length;i++)
-        {
-            //executa um comando por vez
-            let entradaAtual = structuredClone(entrada);
-            entradaAtual = lista[i];
+    function play(){
+        setExecucao(true);
+        setProx(0);
+    }
 
-            setSaida(entradaAtual);
-
-            const tokens = entradaAtual.split(" ");
-            if(tokens[0] === "mover"){
-                const x  = Number(tokens[1]);
-                const y  = Number(tokens[2]);
-                console.log(`Movendo para ${x} ${y}`);
-                onPosicaoChange({x,y});
-    
-            }
-            if(!refInput.current) return;
-            refInput.current.select();
+    useEffect(()=>{
+        if(!refRelogio.current && execucao){
+            console.log("Inicia relógio");
+            refRelogio.current = setInterval(()=>{
+                console.log(lista[prox]);
+                setProx((p)=>p+1);
+            },3000);
         }
-        setTimeout(()=>5000);
+        return ()=>{
+            if(!execucao){
+                console.log("Cancela relógio");
+                clearInterval(refRelogio.current);
+                refRelogio.current = 0;
+            }
+        }
+
+    },[execucao, prox])
+
+    function stop(){
+        setExecucao(false);
+    }
+
+
+    function executaLista(){
+        console.log(lista[prox]);
+    
+        // for(let i=0;i<lista.length;i++)
+        // {
+        //     //executa um comando por vez
+        //     let entradaAtual = structuredClone(entrada);
+        //     entradaAtual = lista[i];
+
+        //     setSaida(entradaAtual);
+
+        //     const tokens = entradaAtual.split(" ");
+        //     if(tokens[0] === "mover"){
+        //         const x  = Number(tokens[1]);
+        //         const y  = Number(tokens[2]);
+        //         console.log(`Movendo para ${x} ${y}`);
+        //         onPosicaoChange({x,y});
+    
+        //     }
+        //     if(!refInput.current) return;
+        //     refInput.current.select();
+        //     setTimeout(()=>5000);
+        // };
+        
     }
 
     function onExecutarClick()
@@ -53,7 +87,8 @@ export default function Prompt({onPosicaoChange}:PromptProps){
 
         <ol className="listaDeComandos">{lista}</ol>
 
-        <button onClick={executaLista}>play</button>
+        <button onClick={play}>play</button>
+        <button onClick={stop}>stop</button>
     </div>
     )
 }
